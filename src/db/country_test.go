a7916c52df6ec295ad4993c4ad89f706a40f57be
syncore <syncore@syncore.org>
2015-12-31 04:34:45 -0500

    Added country database test

---
+++ src/db/country_test.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"os"
+	"steamtest/src/models"
+	"strings"
+	"testing"
+)
+
+func init() {
+	// need base directory for config and other files
+	err := os.Chdir("../..")
+	if err != nil {
+		panic("Unable to change directory for tests")
+	}
+}
+
+func TestOpenCountryDB(t *testing.T) {
+	db, err := OpenCountryDB()
+	// Will panic anyway
+	if err != nil {
+		t.Fatalf("Error opening country database: %s", err)
+	}
+	defer db.Close()
+}
+
+func TestGetCountryInfo(t *testing.T) {
+	cdb, err := OpenCountryDB()
+	if err != nil {
+		t.Fatalf("Error opening country database: %s", err)
+	}
+	defer cdb.Close()
+	c := make(chan *models.DbCountry, 1)
+	ip := "192.211.62.11"
+	cinfo := &models.DbCountry{}
+	go GetCountryInfo(c, cdb, ip)
+	cinfo = <-c
+	if !strings.EqualFold(cinfo.CountryCode, "US") {
+		t.Fatalf("Expected country code to be US for IP: %s, got: %s",
+			ip, cinfo.CountryCode)
+	}
+	ip = "89.20.244.197"
+	cinfo = &models.DbCountry{}
+	go GetCountryInfo(c, cdb, ip)
+	cinfo = <-c
+	if !strings.EqualFold(cinfo.CountryCode, "NO") {
+		t.Fatalf("Expected country code to be NO for IP: %s, got: %s",
+			ip, cinfo.CountryCode)
+	}
+}
